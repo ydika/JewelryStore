0d686b025b93042be788c0264ffc09596c962933
dikanyuri@tut.by <dikanyuri@tut.by>
2021-06-01 19:53:29 +0300

    Add dbeditor partial views.

--- JewelryStore/Controllers/DBChangeringController.cs
+++ JewelryStore/Controllers/DBChangeringController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using JewelryStore.Models;
+using JewelryStore.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +12,70 @@ using System.Threading.Tasks;
 
 namespace JewelryStore.Controllers
 {
+    [Route("[controller]")]
     [Authorize(Roles = "Admin")]
     public class DBChangeringController : Controller
     {
+        private readonly DataBaseContext dbContext;
+        private readonly UserManager<UserModel> _userManager;
+
+        public DBChangeringController(DataBaseContext context, UserManager<UserModel> userManager)
+        {
+            dbContext = context;
+            _userManager = userManager;
+        }
+
+        [Route("[action]")]
         public IActionResult DataBaseEditor()
         {
-            return View();
+            dbContext.Jewelries.Load();
+
+            return View(dbContext.Jewelries.ToList());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetJewelrysTable()
+        {
+            await dbContext.Jewelries.LoadAsync();
+
+            return PartialView("_JewelrysTable", await dbContext.Jewelries.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetCharacteristicsTable()
+        {
+            await dbContext.Characteristics.Include(x => x.CharacteristicValues).LoadAsync();
+
+            return PartialView("_CharacteristicsTable", await dbContext.Characteristics.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetJewelryKindsTable()
+        {
+            await dbContext.JewelryKinds.LoadAsync();
+
+            return PartialView("_JewelryKinds", await dbContext.JewelryKinds.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetDiscountsTable()
+        {
+            await dbContext.Discounts.LoadAsync();
+
+            return PartialView("_DiscountsTable", await dbContext.Discounts.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetUsersTable()
+        {
+            await dbContext.Users.LoadAsync();
+
+            return PartialView("_UsersTable", await dbContext.Users.ToListAsync());
         }
     }
 }
---
+++ JewelryStore/wwwroot/inline/js/databaseeditor.js
@@ -0,0 +1,35 @@
+﻿function GetJewelrysTable(obj) {
+    $('main').addClass('loading');
+    GetTable('getjewelrystable', obj);
+}
+
+function GetCharacteristicsTable(obj) {
+    $('main').addClass('loading');
+    GetTable('getcharacteristicstable', obj);
+}
+
+function GetJewelryKindsTable(obj) {
+    $('main').addClass('loading');
+    GetTable('getjewelrykindstable', obj);
+}
+
+function GetDiscountsTable(obj) {
+    $('main').addClass('loading');
+    GetTable('getdiscountstable', obj);
+}
+
+function GetUsersTable(obj) {
+    $('main').addClass('loading');
+    GetTable('getuserstable', obj);
+}
+
+function GetTable(url, obj) {
+    $.get(`${url}`,
+        function (data) {
+            $('.active').addClass('link-dark').removeClass('active');
+            $('#dbTable').html(data);
+            $(obj).addClass('active');
+            $('main').removeClass('loading');
+        }
+    );
+}
